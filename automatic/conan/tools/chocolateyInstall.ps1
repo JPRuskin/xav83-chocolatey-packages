40f7a3de58284093d068a651046c52cf0ce6b76e
Xav83 <x.jouvenot@gmail.com>
2020-05-10 23:24:56 +0200

    [AU conan:1.25.0]

---
+++ automatic/conan/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'conan'
+$url32       = 'https://github.com/conan-io/conan/releases/download/1.25.0/conan-win-32_1_25_0.exe'
+$url64       = 'https://github.com/conan-io/conan/releases/download/1.25.0/conan-win-64_1_25_0.exe'
+$checksum32  = '9e1cf76f3cfa9ff74c6bcee8c1393e17cd0f39855055ce7e910ea7c823a4e9fe'
+$checksum64  = '8c7fce0fcfb660831de55bebac5f2d0ab8700ae9089b13135d34df988cbb633e'
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
