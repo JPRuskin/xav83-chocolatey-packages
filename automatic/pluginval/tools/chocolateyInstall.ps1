3434cb4e4b4940f0a2216b5bec7115a58303ab84
Xav83 <x.jouvenot@gmail.com>
2020-05-17 22:49:30 +0200

    [AU pluginval:0.2.6]

---
+++ automatic/pluginval/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = $env:ChocolateyPackageName
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'ZIP'
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  validExitCodes = @(0)
+}
+Install-ChocolateyZipPackage @packageArgs
