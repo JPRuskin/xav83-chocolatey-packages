478946d94c8c3c10399323021ba7e31b7cb49b09
Xav83 <x.jouvenot@gmail.com>
2020-05-18 09:07:18 +0200

    [AU cairoshell:0.3.7015]

---
+++ automatic/cairoshell/tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = 'cairoshell'
+$url32       = ''
+$url64       = ''
+$checksum32  = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'EXE'
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/VERYSILENT'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/cairoshell/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/cairoshell/cairoshell/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "CairoSetup_(32|64)bit.exe$"
+    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
+
+    $version = $url[0] -split '/' | select -Last 1 -Skip 1 | tr -d v
+    $url32 = 'https://github.com' + $url[0]
+    $url64 = 'https://github.com' + $url[1]
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update
