17877a5ec1203afcde51460c1ef2c81fbc9881a1
Xav83 <x.jouvenot@gmail.com>
2020-05-21 11:47:34 +0200

    [AU phantombot:3.1.2]

---
+++ automatic/phantombot/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$packageName = $env:ChocolateyPackageName
+$url32       = ''
+$checksum32  = ''
+
+$packageArgs = @{
+  packageName    = $packageName
+  fileType       = 'ZIP'
+  url            = $url32
+  checksum       = $checksum32
+  checksumType   = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  validExitCodes = @(0)
+}
+Install-ChocolateyZipPackage @packageArgs
---
+++ automatic/phantombot/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/PhantomBot/PhantomBot/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "PhantomBot.+.zip$"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = $url -split '/' | select -Last 1 -Skip 1 | tr -d v
+    $url32 = 'https://github.com' + $url
+
+    $Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update
