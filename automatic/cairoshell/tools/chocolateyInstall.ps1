c595ad334e5c7b19a9b8e74480f6f74694281a2a
Xav83 <x.jouvenot@gmail.com>
2020-05-21 10:02:02 +0200

    [AU cairoshell:0.3.7015]
    Finds the correct option to silent cairoshell installer

--- automatic/cairoshell/tools/chocolateyInstall.ps1
+++ automatic/cairoshell/tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ $packageArgs = @{
   checksum64     = $checksum64
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
-  silentArgs     = '/VERYSILENT'
+  silentArgs     = '/S'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
