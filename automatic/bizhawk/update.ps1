c63aa479732709fb734b04bde87e9f514235d0f0
Xav83 <x.jouvenot@gmail.com>
2020-06-10 19:46:11 +0200

    [AU bizhawk:2.4.2]

---
+++ automatic/bizhawk/update.ps1
@@ -0,0 +1,27 @@
+import-module au
+
+$releases = 'https://github.com/TASVideos/BizHawk/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "BizHawk-.+.zip$"
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    $url64 = 'https://github.com' + $url
+
+    $Latest = @{ URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 64
