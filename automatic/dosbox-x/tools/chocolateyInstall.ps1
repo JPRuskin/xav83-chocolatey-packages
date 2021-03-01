b5ac6937d8e47715c350d9efc1a85f677bb417da
Xav83 <x.jouvenot@gmail.com>
2021-03-01 23:30:37 +0100

    [AU dosbox-x]

--- automatic/dosbox-x/tools/chocolateyInstall.ps1
+++ automatic/dosbox-x/tools/chocolateyInstall.ps1
@@ -1,15 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = $env:ChocolateyPackageName
-$url32       = ''
-$checksum32  = ''
+$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.11/dosbox-x-windows-0.83.11-setup.exe'
+$checksum32  = 'e1355518f0f7752f725b021fa08e3ad84956e1b1866e0616b77531b39cdf0d83'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $Env:ChocolateyPackageName
   fileType       = 'EXE'
-  url32Bit       = $url32
-  checksum32     = $checksum32
-  checksumType32 = 'sha256'
+  url       = $url32
+  checksum     = $checksum32
+  checksumType = 'sha256'
   silentArgs     = '/VERYSILENT'
   validExitCodes = @(0)
 }
