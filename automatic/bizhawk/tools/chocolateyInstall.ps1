bd60c2f4c6a8607fed18536e357a1dafc3ccd361
Xav83 <x.jouvenot@gmail.com>
2021-03-01 22:08:13 +0100

    [AU bizhawk]

--- automatic/bizhawk/tools/chocolateyInstall.ps1
+++ automatic/bizhawk/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$url64       = ''
-$checksum64  = ''
+$url64       = 'https://github.com/TASVideos/BizHawk/releases/download/2.6.1/BizHawk-2.6.1-win-x64.zip'
+$checksum64  = 'b2c15a03ce37f6807797a7d80a073c7fcf9d2365cb92d6828495e62e113f6d7e'
 
 $packageArgs = @{
   packageName    = $packageName
