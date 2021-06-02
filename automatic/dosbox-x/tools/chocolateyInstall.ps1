609542dc5871b55e9999091c82eaf8eca89efb12
Chocolatey <chocolatey@realdimensions.net>
2021-06-02 00:05:59 +0000

    AU: 1 updated - dosbox-x [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/0e63591780ea2768ac67266efcbddd1f5a4b779c

--- automatic/dosbox-x/tools/chocolateyInstall.ps1
+++ automatic/dosbox-x/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.11/dosbox-x-windows-0.83.11-setup.exe'
-$checksum32  = 'e1355518f0f7752f725b021fa08e3ad84956e1b1866e0616b77531b39cdf0d83'
+$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.14/dosbox-x-windows-0.83.14-setup.exe'
+$checksum32  = '9547250ac5db83cb1e2c10433df59ab2efdfa0c97b541199ea041f2b07d05df8'
 
 $packageArgs = @{
   packageName    = $Env:ChocolateyPackageName
