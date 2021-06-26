f71f49e2408e6c51e86f04ff74ef1ffc5e35870f
Chocolatey <chocolatey@realdimensions.net>
2021-06-26 00:06:11 +0000

    AU: 1 updated - doxybook2 [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/d7625026feb2c3410a30264a4974cf6f9f127355

--- automatic/doxybook2/tools/chocolateyInstall.ps1
+++ automatic/doxybook2/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/matusnovak/doxybook2/releases/download/v1.3.5/doxybook2-windows-win32-v1.3.5.zip'
+$url64       = 'https://github.com/matusnovak/doxybook2/releases/download/v1.3.5/doxybook2-windows-win64-v1.3.5.zip'
+$checksum32  = 'd340177746d8a0bef7b370948f9434af60d91a2bb50c79c8c9f0e30cba6709a1'
+$checksum64  = 'b6215797b526443d6f98d69e0a263a48470b1211717c30a5dc08464e3bd214ba'
 
 $packageArgs = @{
   packageName    = $packageName
