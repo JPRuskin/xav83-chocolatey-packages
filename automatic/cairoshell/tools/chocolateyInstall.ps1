c97e1172f1458179ccf0c77b2ae53f536202c8c3
Chocolatey <chocolatey@realdimensions.net>
2021-07-02 08:06:19 +0000

    AU: 1 updated - cairoshell [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/799d882126179c6e17259b7b7402905d8f4f1d12

--- automatic/cairoshell/tools/chocolateyInstall.ps1
+++ automatic/cairoshell/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'cairoshell'
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/cairoshell/cairoshell/releases/download/v0.4.133/CairoSetup_32bit.exe'
+$url64       = 'https://github.com/cairoshell/cairoshell/releases/download/v0.4.133/CairoSetup_64bit.exe'
+$checksum32  = '34edfaebb6bb34368fb65f8ae438c1d743d7cedf38056b2bf3bc6d847bff5f39'
+$checksum64  = 'c6408e7c9ec64e625526b758fa93da178c8e81bac9aef8853d016d1fc6fcc9eb'
 
 $packageArgs = @{
   packageName    = $packageName
