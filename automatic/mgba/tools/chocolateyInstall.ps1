118b9f4b0676d4845fb8a3d260b2db3bf0a5540d
Chocolatey <chocolatey@realdimensions.net>
2021-07-11 08:05:57 +0000

    AU: 1 updated - mgba [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/f8aabca0d084e02d9adbff4bdd866bde3d1f6283

--- automatic/mgba/tools/chocolateyInstall.ps1
+++ automatic/mgba/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'mgba'
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/mgba-emu/mgba/releases/download/0.9.2/mGBA-0.9.2-win32-installer.exe'
+$url64       = 'https://github.com/mgba-emu/mgba/releases/download/0.9.2/mGBA-0.9.2-win64-installer.exe'
+$checksum32  = '974ea1f358077761e4488898fba620d7e12dce7911d6f0c051d4071822c23183'
+$checksum64  = 'd7b7fcf239e3c487c2250782ef896156bf3c44c009f9236afb59268c7684c61d'
 
 $packageArgs = @{
   packageName    = $packageName
