95efec3d4d3a6d47bd359e704cfc071d8432eba6
Xav83 <x.jouvenot@gmail.com>
2021-07-14 23:21:27 +0200

    [AU tvrename]

--- automatic/cairoshell/tools/chocolateyInstall.ps1
+++ automatic/cairoshell/tools/chocolateyInstall.ps1
@@ -15,7 +15,7 @@ $packageArgs = @{
   checksum64     = $checksum64
   checksumType   = 'sha256'
   checksumType64 = 'sha256'
-  silentArgs     = '/S'
+  silentArgs     = '/SD'
   validExitCodes = @(0)
 }
 Install-ChocolateyPackage @packageArgs
