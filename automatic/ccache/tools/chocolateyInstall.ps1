0670ef58c264c89d4ffcb515b854a62657c8044e
Chocolatey <chocolatey@realdimensions.net>
2021-08-20 00:06:22 +0000

    AU: 1 updated - ccache [skip ci] https://gist.github.com/c4eb33a8aa993629f3d047747322e809/e6950a91df6bf8ddfdc76a787565cdc27fdea26a

--- automatic/ccache/tools/chocolateyInstall.ps1
+++ automatic/ccache/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = $env:ChocolateyPackageName
-$url32       = ''
-$url64       = ''
-$checksum32  = ''
-$checksum64  = ''
+$url32       = 'https://github.com/ccache/ccache/releases/download/v4.4/ccache-4.4-windows-32.zip'
+$url64       = 'https://github.com/ccache/ccache/releases/download/v4.4/ccache-4.4-windows-64.zip'
+$checksum32  = 'ca96d86cd138681673412d65e9e0449a93a9648d185f5e3e68dc9a14e5377d9d'
+$checksum64  = 'f6a9bf26ed501da666fd0067a47da96e35b74c91c3d367fdd7b309ead0accebe'
 
 $packageArgs = @{
   packageName    = $packageName
