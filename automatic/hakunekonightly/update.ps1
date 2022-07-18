0f231c5a2aa972168558a76803a02db9468b6a4a
Xav83 <x.jouvenot@gmail.com>
2022-07-18 14:53:01 +0200

    [AU hakunekonightly]

---
+++ automatic/hakunekonightly/update.ps1
@@ -0,0 +1,30 @@
+import-module au
+
+$releases = 'https://github.com/manga-download/hakuneko/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "hakuneko-desktop_.+_windows-setup_(i386|amd64).exe"
+    $url = $download_page.links | ? href -match $re | select -First 2 -expand href
+
+    $version = $url[0] -split '_' | select -First 1 -Skip 1
+    $url64 = 'https://github.com' + $url[0]
+    $url32 = 'https://github.com' + $url[1]
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update
