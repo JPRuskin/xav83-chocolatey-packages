40a3f57d1730bf32177956ef6ede579276861123
Xav83 <x.jouvenot@gmail.com>
2022-07-29 11:07:49 +0200

    [AU keepassxc-legacy]

---
+++ automatic/keepassxc-legacy/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'msi'
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = "/quiet"
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
