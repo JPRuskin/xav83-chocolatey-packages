c16314251208f156bfc29fd3b909b4a0456a7756
Xav83 <x.jouvenot@gmail.com>
2022-07-30 10:55:37 +0200

    [AU comictagger]

---
+++ automatic/comictagger/update.ps1
@@ -0,0 +1,32 @@
+import-module au
+
+$releases = 'https://github.com/comictagger/comictagger/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "ComicTagger-.+-win-AMD64.exe.zip"
+
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = $url -split '/' | select -Last 1 -Skip 1
+    if ($version.Contains("beta")) { 
+        $version = $version.remove($version.Length - 2, 1)
+    }
+
+    $url64 = 'https://github.com' + $url
+
+    $Latest = @{ URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 64
