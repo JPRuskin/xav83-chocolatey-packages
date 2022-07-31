1bb8d1f57b2551e8ba50f2f626024c0dd9a2c287
Xav83 <x.jouvenot@gmail.com>
2022-07-31 10:46:19 +0200

    [AU glossi]

---
+++ automatic/glossi/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
+  validExitCodes = @(0)
+}
+Install-ChocolateyZipPackage @packageArgs
