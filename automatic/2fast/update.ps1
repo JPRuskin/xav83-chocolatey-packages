0572db87630fa23b5c87e6b84033a4bded40e3cd
Xav83 <x.jouvenot@gmail.com>
2022-08-14 12:28:33 +0200

    [AU 2fast]

---
+++ automatic/2fast/update.ps1
@@ -0,0 +1,28 @@
+import-module au
+
+$releases = 'https://github.com/2fast-team/2fast/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+
+    $re  = "Project2FA.UWP_1.1.0.0.zip"
+
+    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+    $version = ($url -split '/' | select -Last 1 -Skip 1) -replace 'v',''
+    $url64 = 'https://github.com' + $url
+
+    $Latest = @{ URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 64
