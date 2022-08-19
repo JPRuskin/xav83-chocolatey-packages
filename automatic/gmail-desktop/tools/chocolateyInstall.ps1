d7f443ac28e4ad68352a4d76ce760af51f78f3fd
Xav83 <x.jouvenot@gmail.com>
2022-08-20 00:27:04 +0200

    [AU gmail-desktop]

---
+++ automatic/gmail-desktop/tools/chocolateyInstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url64       = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url64Bit       = $url64
+  checksum64     = $checksum64
+  checksumType64 = 'sha256'
+  silentArgs     = ''
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
