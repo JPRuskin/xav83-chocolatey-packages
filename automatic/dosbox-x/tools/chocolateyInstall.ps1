eee250d21c81812c67cb7717c220e2a529e5a53b
Xav83 <x.jouvenot@gmail.com>
2022-09-02 23:55:28 +0200

    Removes dosbox old url32 informations

--- automatic/dosbox-x/tools/chocolateyInstall.ps1
+++ automatic/dosbox-x/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$url32       = 'https://github.com/joncampbell123/dosbox-x/releases/download/dosbox-x-v0.83.14/dosbox-x-windows-0.83.14-setup.exe'
+$url32       = ''
 $url64       = ''
-$checksum32  = '9547250ac5db83cb1e2c10433df59ab2efdfa0c97b541199ea041f2b07d05df8'
+$checksum32  = ''
 $checksum64  = ''
 
 $packageArgs = @{
