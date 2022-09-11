6a216b9d3e478e00d9134124544da49a3e7e5724
Xav83 <x.jouvenot@gmail.com>
2022-09-11 11:59:37 +0200

    Adds the possibility to pass the path type as an argument during conan installation

--- automatic/conan/tools/chocolateyInstall.ps1
+++ automatic/conan/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = 'conan'
-$url32       = 'https://github.com/conan-io/conan/releases/download/1.39.0/conan-win-32.exe'
-$url64       = 'https://github.com/conan-io/conan/releases/download/1.39.0/conan-win-64.exe'
-$checksum32  = 'ce3dbb37465eb6190b0aa7da66ee307d9a413bea59a3edf49d252efb8fda1ece'
-$checksum64  = '59d0b0b3f4418cd7846160a00ff2e1f2c2e5c6c665836d52954c1c25af213d73'
+$packageName = ''
+$url32       = ''
+$url64       = ''
+$checksum32  = ''
+$checksum64  = ''
 
 $packageArgs = @{
   packageName    = $packageName
@@ -21,5 +21,7 @@ $packageArgs = @{
 Install-ChocolateyPackage @packageArgs
 
 # Add conan to PATH
+if (!Get-PackageParameters['PathType']) { Get-PackageParameters['PathType'] = 'Process' }
+
 $conan_path = "C:\Program Files\Conan\conan"
-Install-ChocolateyPath -PathToInstall $conan_path -PathType Process
+Install-ChocolateyPath -PathToInstall $conan_path -PathType Get-PackageParameters['PathType']
