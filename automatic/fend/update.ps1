3eab9b52482256aa5003edddc47d29c03d626275
Xav83 <x.jouvenot@gmail.com>
2022-10-09 17:04:22 +0200

    Corrects the update script of the fend package.

--- automatic/fend/update.ps1
+++ automatic/fend/update.ps1
@@ -12,11 +12,15 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-    $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $releases_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $release_tag_url = "https://github.com" + ($releases_page.Links.Href -match "/tag/" | Select-Object -First 1)
+
+    $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
+    $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
 
     $re  = "fend-.+-windows-x64-exe.zip"
 
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url = $assets_page.Links.Href -match $re | select -First 1
 
     $version = ($url -split '/' | select -Last 1 -Skip 1) -replace 'v',''
     $url64 = 'https://github.com' + $url
