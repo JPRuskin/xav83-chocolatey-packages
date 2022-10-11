abc2a13bffbf8acb57d18d01c202441caefc9360
Xav83 <x.jouvenot@gmail.com>
2022-10-11 22:04:54 +0200

    [AU 2fast:1.2.0]

--- automatic/2fast/tools/chocolateyInstall.ps1
+++ automatic/2fast/tools/chocolateyInstall.ps1
@@ -5,11 +5,9 @@ $checksum64  = ''
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
-  fileType       = 'ZIP'
   url64Bit       = $url64
   checksum64     = $checksum64
   checksumType64 = 'sha256'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
-  validExitCodes = @(0)
 }
 Install-ChocolateyZipPackage @packageArgs
