04488180eba84ea5da3da474c2f5017fba7382d5
Xav83 <x.jouvenot@gmail.com>
2022-11-17 23:02:48 +0100

    [AU hopsan:2.20.0]

---
+++ automatic/hopsan/tools/chocolateyInstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$url32       = ''
+$url64       = ''
+$checksum32  = ''
+$checksum64  = ''
+
+$packageArgs = @{
+  packageName    = $env:ChocolateyPackageName
+  fileType       = 'EXE'
+  url            = $url32
+  url64Bit       = $url64
+  checksum       = $checksum32
+  checksum64     = $checksum64
+  checksumType   = 'sha256'
+  checksumType64 = 'sha256'
+  silentArgs     = '/SILENT'
+  validExitCodes = @(0)
+}
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/hopsan/update.ps1
@@ -0,0 +1,35 @@
+import-module au
+
+$releases = 'https://github.com/Hopsan/hopsan/releases'
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+    $releases_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
+    $release_tag_url = "https://github.com" + ($releases_page.Links.Href -match "/tag/" | Select-Object -First 1)
+
+    $expanded_assets_url = $release_tag_url -replace "/tag/","/expanded_assets/"
+    $assets_page = Invoke-WebRequest -Uri $expanded_assets_url -UseBasicParsing
+
+    $re  = "Hopsan-.+-win(32|64)-with_compiler-installer.exe"
+    $url = $assets_page.Links.Href -match $re | select -First 2
+
+    $version = ($url[0] -split '/' | select -Last 1 -Skip 1) -replace 'v',''
+
+    $url32 = 'https://github.com' + $url[0]
+    $url64 = 'https://github.com' + $url[1]
+
+    $Latest = @{ URL32 = $url32; URL64 = $url64; Version = $version }
+    return $Latest
+}
+
+update -ChecksumFor 64
