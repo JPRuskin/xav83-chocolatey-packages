c729d292010ca52346aa09df768dddb802e33698
Xav83 <x.jouvenot@gmail.com>
2023-01-11 09:05:57 +0100

    Removes an intermediate variable for the package name.

--- automatic/bizhawk/tools/chocolateyInstall.ps1
+++ automatic/bizhawk/tools/chocolateyInstall.ps1
@@ -1,11 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$packageName = $env:ChocolateyPackageName
 $url64       = 'https://github.com/TASVideos/BizHawk/releases/download/2.6.3/BizHawk-2.6.3-win-x64.zip'
 $checksum64  = '374076fbbd53c65cce6aea670f516050084b881e025e2ff5b9b0df54fd0dcc41'
 
 $packageArgs = @{
-  packageName    = $packageName
+  packageName    = $env:ChocolateyPackageName
   fileType       = 'ZIP'
   url64Bit       = $url64
   checksum64     = $checksum64
